0b00f3172f9cf797513199319a612d1e64b46ab1
Héctor López <hector.lopez@DSA-DEV2-10>
2019-05-07 08:47:02 -0500

    removing unnecessary

--- Bet/MainWindow.xaml.cs
+++ Bet/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Bet
+﻿namespace Bet
 {
+    using System.Windows;
+
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
@@ -27,8 +14,8 @@ namespace Bet
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Bet.Logica.Logica logica = new Logica.Logica();
-            logica.Test();
+            Logica.Logica logica = new Logica.Logica();
+            //logica.Test();
         }
     }
 }
